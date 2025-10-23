d89e5029915320d9f17678c33dd4320510fa5daa
mijhail <mijhailtovar@gmail.com>
2025-10-23 15:40:05 -0400

    commit para subir este proyecto en la nube y usarlo para el portafolio

--- src/Global.js
+++ src/Global.js
@@ -1,5 +1,10 @@
 var Global = {
-    url:'http://localhost:3900/api/'
+    // Lee la variable de entorno, y si no existe (local), usa localhost
+    // Vercel inyectará VUE_APP_API_URL
+    url: process.env.VUE_APP_API_URL || 'http://localhost:3900'
 }
+
+// Asegúrate de que la URL termine en /api/ para mantener la consistencia en tu código
+Global.url = Global.url + '/api/';
 
 export default Global;
